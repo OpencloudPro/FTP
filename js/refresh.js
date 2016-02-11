625115442d620a7f747a0d7d37033fe88f0b17f7
jgopencloud <jg@opencloud.pro>
2016-02-11 15:53:02 +0000

    SEB refresh div

---
+++ js/refresh.js
@@ -0,0 +1,20 @@
+function autoRefresh_div()
+		{
+		try{
+
+			var html = document.documentElement;
+			var lang = html.getAttribute('lang').replace('-', '_');
+			if (lang == 'pt_PT'){
+                $("#refresh").load("/tempos #refresh");
+			}
+			if (lang != 'pt_PT'){
+                $("#refresh").load("/en_US/tempos #refresh");
+			}
+
+		
+		}
+		catch(e){
+		}
+		}
+
+		setInterval('autoRefresh_div()', 60000); // refresh div after 1 secs
